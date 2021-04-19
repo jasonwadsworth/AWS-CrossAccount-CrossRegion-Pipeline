e8fed0d700260504734be13fef3df32d7238f88b
Rick Stricker <rick@mobileupsoftware.com>
2021-04-19 10:31:19 -0500

    use AddNewtonsoftJson for serialization

--- LambdaFunction/src/LambdaFunction/Startup.cs
+++ LambdaFunction/src/LambdaFunction/Startup.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 
 namespace LambdaFunction
 {
@@ -49,7 +39,8 @@ namespace LambdaFunction
                 .AddMvc(options =>
                 {
                     options.EnableEndpointRouting = false;
-                });
+                })
+                .AddNewtonsoftJson();
         }
 
         /// <summary>
